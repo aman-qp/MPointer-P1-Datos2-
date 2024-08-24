92b3825193b189b2f8f971b3beaea701ebb59304
aman-qp <amaquesada@estudiantec.cr>
2024-08-23 21:06:09 -0600

    Cambios en el manejo de archivos

--- MPointer.h
+++ MPointer.h
@@ -1,7 +1,6 @@
 #ifndef MPOINTER_H
 #define MPOINTER_H
-
-#include "MPointerGC.h"  // Incluir el gestor de memoria
+#include "MPointerGC.h"
 
 template<typename T>
 class MPointer {
@@ -11,21 +10,53 @@ private:
 
 public:
     // Constructor por defecto
-    MPointer();
+    MPointer() : ptr(nullptr), id(-1) {}
 
-    // Método estático para crear un nuevo MPointer
-    static MPointer<T> New();
+    // Metodo estático para crear un nuevo MPointer
+    static MPointer<T> New() {
+        MPointer<T> mp;
+        mp.ptr = new T;  // Asignar memoria para el tipo T
+        mp.id = MPointerGC::GetInstance().AddPointer(&mp);  // Registrar el puntero en MPointerGC
+        return mp;
+    }
 
     // Sobrecarga del operador *
-    T& operator*();
+    T& operator*() {
+        return *ptr;  // Devolver el valor referenciado
+    }
 
-    // No sobrecargaremos el operador &, ya que Clang-Tidy lo desaconseja
+    // Sobrecarga del operador &
+    T* operator&() {
+        return ptr;  // Devolver la dirección del puntero
+    }
 
     // Sobrecarga del operador =
-    MPointer<T>& operator=(const MPointer<T>&);
+    MPointer<T>& operator=(const MPointer<T>& other) {
+        if (this != &other) {
+            // Si este MPointer ya apuntaba a otro objeto, liberar memoria y reducir la referencia
+            if (this->ptr != nullptr) {
+                MPointerGC::GetInstance().RemovePointer(this->id);
+                delete this->ptr;
+            }
+
+            this->ptr = other.ptr;
+            this->id = other.id;
+
+            // Incrementar el contador de referencias para el nuevo puntero
+            MPointerGC::GetInstance().IncrementRefCount(this->id);
+        }
+        return *this;
+    }
 
     // Destructor
-    ~MPointer();
+    ~MPointer() {
+        if (this->ptr != nullptr) {
+            // Informar a MPointerGC que este puntero se está destruyendo
+            if (MPointerGC::GetInstance().RemovePointer(this->id)) {
+                delete ptr;  // Solo eliminar si no hay más referencias
+            }
+        }
+    }
 };
 
 #endif  // MPOINTER_H
--- MPointerGC.h
+++ MPointerGC.h
@@ -2,11 +2,14 @@
 #define MPOINTERGC_H
 
 #include <unordered_map>
+#include <mutex>
 
 class MPointerGC {
 private:
     std::unordered_map<int, void*> pointers;  // Mapa de punteros gestionados
+    std::unordered_map<int, int> refCounts;   // Mapa de contadores de referencias
     int nextId;  // Siguiente ID disponible para asignar
+    std::mutex mtx;  // Mutex para proteger el acceso a los mapas
 
     // Constructor privado para el patrón Singleton
     MPointerGC() : nextId(0) {}
@@ -20,14 +23,33 @@ public:
 
     // Agrega un puntero al gestor y devuelve un ID único
     int AddPointer(void* ptr) {
+        std::lock_guard<std::mutex> lock(mtx);
         int id = nextId++;
         pointers[id] = ptr;
+        refCounts[id] = 1;  // Inicialmente una referencia
         return id;
     }
 
-    // Elimina un puntero del gestor
-    void RemovePointer(int id) {
-        pointers.erase(id);
+    // Incrementa el contador de referencias para un puntero
+    void IncrementRefCount(int id) {
+        std::lock_guard<std::mutex> lock(mtx);
+        if (refCounts.find(id) != refCounts.end()) {
+            refCounts[id]++;
+        }
+    }
+
+    // Elimina un puntero del gestor y devuelve true si ya no hay más referencias
+    bool RemovePointer(int id) {
+        std::lock_guard<std::mutex> lock(mtx);
+        if (refCounts.find(id) != refCounts.end()) {
+            refCounts[id]--;
+            if (refCounts[id] == 0) {
+                pointers.erase(id);
+                refCounts.erase(id);
+                return true;  // No hay más referencias
+            }
+        }
+        return false;  // Aún hay referencias
     }
 
     // Prohíbe la copia y asignación del singleton
